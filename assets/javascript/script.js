9c704580cdd0a604bb4ecdb4c9faac3154947d44
Mimolet <samuel.kl@ludus-academie.com>
2023-04-27 17:53:47 +0200

    Working Dark Mode (part 1)

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,21 +3,42 @@ $(document).ready(function(){
   let language_display = false;
   let language = "english";
 
-  console.log("The code is running perfectly");
+  console.log("The code has begun to run");
   
+  function dark_mode_off(){
+    dark_mode = false;
+    $(".dark-mode-on").hide();
+    $(".dark-mode-off").show();
+
+    $("*").css({"color":"black"});
+    $("body").css({"background-color":"rgb(235, 235, 235)"});
+    $("main").css({"background-color":"rgb(255, 255, 255)","box-shadow": "0px 0px 7px 1px gray"});
+    $(".top-buttons button").css({"background-color":"rgba(255, 255, 255, 0.7)"}); 
+    $(".languages-dropdown div").css({"background-color":"rgba(243, 241, 239, 0.8)"});
+    $(".profile_pic").css({"border": "solid rgb(243, 241, 239) 5px"});
+
+  };function dark_mode_on(){
+    dark_mode = true;
+    $(".dark-mode-off").hide();
+    $(".dark-mode-on").show();
+
+    $("*").css({"color":"rgb(220, 220, 220)"}); 
+    $("body").css({"background-color":"rgb(36, 36, 36)"});
+    $("main").css({"background-color":"rgb(51, 51, 51)","box-shadow": "0px 0px 7px 1px gray"});
+    $(".top-buttons button").css({"background-color":"rgba(51, 51, 51, 0.7)"});
+    $(".languages-dropdown div").css({"background-color":"rgba(51, 51, 51, 0.8)"});
+    $(".profile_pic").css({"border": "solid rgb(51, 51, 51) 5px"});
+  };
+
   $(".languages-dropdown").hide();
   $(".french").hide();
   
   $(".dark-mode-btn").click(function(){
     if(dark_mode === false){
-      $(".dark-mode-off").hide();
-      $(".dark-mode-on").show();
-      dark_mode = true;
+      dark_mode_on();
       console.log("Dark mode on");
     } else {
-      $(".dark-mode-on").hide();
-      $(".dark-mode-off").show();
-      dark_mode = false;
+      dark_mode_off();
       console.log("Dark mode off");
     }
   });
@@ -61,15 +82,3 @@ $(document).ready(function(){
   });
 
 });
-
-/*if(language === "english"){
-      $(".english").hide();
-      $(".french").show();
-      language = "french";
-      console.log("Language is now set to french");
-    } else {
-      $(".french").hide();
-      $(".english").show();
-      language = "english";
-      
-    }*/
