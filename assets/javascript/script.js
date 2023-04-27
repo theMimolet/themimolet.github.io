2d239fa46aabd00bbef7f130a3310a12f3baeba6
Mimolet <samuel.kl@ludus-academie.com>
2023-04-28 01:43:11 +0200

    Made the game gallery

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   let dark_mode = false;
   let language_display = false;
   let language = "english";
+  let show_game = "none";
   
 
   // Light mode
@@ -18,10 +19,14 @@ $(document).ready(function(){
     $(".dark-mode-off").show();
 
     $("*").css({"color":"black"});
+    $(".play a span").css({"color":"white"});
     $("body").css({"background-color":"rgb(235, 235, 235)"});
     $("main").css({"background-color":"rgb(255, 255, 255)","box-shadow": "0px 0px 7px 1px gray"});
     $(".daButtons").css({"background-color":"rgba(255, 255, 255, 0.7)"}); 
     $(".socials div").css({"background-color":"rgba(255, 255, 255, 0.7)"});
+    $(".play").css({"background-color":"lime"});
+    $("#game-description table td").css({"border": "solid black 2px"})
+    $("#game-gallery").css({"background-color":"rgba(212, 208, 204, 0.7)"})
     $("#languages-dropdown div").css({"background-color":"rgba(243, 241, 239, 0.8)"});
     $(".profile_pic").css({"border": "solid rgb(255, 255, 255) 5px"});
   };
@@ -38,6 +43,9 @@ $(document).ready(function(){
     $("main").css({"background-color":"rgb(51, 51, 51)","box-shadow": "0px 0px 7px 1px gray"});
     $(".daButtons").css({"background-color":"rgba(51, 51, 51, 0.7)"});
     $(".socials div").css({"background-color":"rgba(51, 51, 51, 0.7)"});
+    $(".play").css({"background-color":"rgb(32, 99, 32)"});
+    $("#game-description table td").css({"border": "solid  2px rgb(200, 200, 200)"})
+    $("#game-gallery").css({"background-color":"rgba(112, 108, 104, 0.7)"})
     $("#languages-dropdown div").css({"background-color":"rgba(51, 51, 51, 0.7)"});
     $(".profile_pic").css({"border": "solid rgb(51, 51, 51) 5px"});
   };
@@ -49,7 +57,9 @@ $(document).ready(function(){
   dark_mode_off(); 
   $("#languages-dropdown").hide();
   $(".french").hide();
-
+  $(".german").hide();
+  $(".save-nautica").hide();
+  $(".totem-destiny").hide();
 
   
   // Interactions avec les boutons
@@ -160,6 +170,42 @@ $(document).ready(function(){
     console.log("Language is now set to german");  
     language = "german"
     };    
+  });
+
+
+  // Jeux   show_game
+
+  function game_reset(){
+    $(".totem-destiny").hide();
+    $(".save-nautica").hide();
+    $("#SN").css({"height":"90%", "box-shadow": "0px 0px 5px 0px black"});
+    $("#TTOD").css({"height":"90%", "box-shadow": "0px 0px 5px 0px black"});
+  };
+
+  $("#SN").click(function(){
+    if (show_game !== "SaveNautica"){
+      game_reset();
+      $(".save-nautica").show();
+      $("#SN").css({"height":"93%", "box-shadow": "0px 0px 15px 0px black"});
+      show_game = "SaveNautica";
+    } else {
+      game_reset();
+      $(".save-nautica").hide();
+      show_game = "none";
+    }
+  });
+
+  $("#TTOD").click(function(){
+    if (show_game !== "TotemDestiny"){
+      game_reset();
+      $(".totem-destiny").show();
+      $("#TTOD").css({"height":"93%", "box-shadow": "0px 0px 15px 0px black"});
+      show_game = "TotemDestiny";
+    } else {
+      game_reset();
+      $(".totem-destiny").hide();
+      show_game = "none";
+    }
   });
 
 });
