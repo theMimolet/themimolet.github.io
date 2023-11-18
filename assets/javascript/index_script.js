32dd6b17117b8f29b33cf1e59ddc9bfafc11d76d
theMimolet <samuel.kl@ludus-academie.com>
2023-11-18 20:10:13 +0100

    New post & news page update

--- assets/javascript/index_script.js
+++ assets/javascript/index_script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   // Variables
 
   let dark_mode = JSON.parse(localStorage.getItem("dark_mode"));
-  let news_version = 2;  
+  let news_version = 3;  
   let show_game = "none";
 
   // Setup 
