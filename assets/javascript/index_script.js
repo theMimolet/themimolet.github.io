3d8350c626f00a7b86396c905e9060c0c76ee496
theMimolet <samuel.kl@ludus-academie.com>
2023-11-30 16:42:09 +0100

    ColourGuess available in italian

--- assets/javascript/index_script.js
+++ assets/javascript/index_script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
   // Variables
 
   let dark_mode = JSON.parse(localStorage.getItem("dark_mode"));
-  let news_version = 4;  
+  let news_version = 5;  
   let show_game = "none";
 
   // Setup 
