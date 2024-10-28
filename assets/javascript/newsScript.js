51cc4c7c3d64b2c22f9dcf3531f77a11c6586cf5
theMimolet <mimolet@mailo.com>
2024-10-28 18:32:31 +0100

    Revert "scriptTest"
    This reverts commit 93e3eae24fc5a260a93069cac4f01cf8606e5a92.

--- assets/javascript/newsScript.js
+++
@@ -1,8 +0,0 @@
-// import data from '../json/newsStorage.json' assert { type: 'json' };
-
-// console.log(data);
-
-fetch('../json/newsStorage.json')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.error('Error:', error));
