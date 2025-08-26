18f90a06211c86e9cd614c5316421097c45fc92c
Mimolet <themimolet@proton.me>
2025-08-26 23:40:20 +0200

    2508.9 - added theme and language auto-select, reworked the theme's system, new post

---
+++ assets/js/darkTheme.js
@@ -0,0 +1,55 @@
+// Variables
+
+let isDarkTheme = JSON.parse(localStorage.getItem("darkTheme"));
+let documentClasses = document.documentElement.classList;
+const colorSchemeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+// ==========================
+
+console.log("Dark theme is " + isDarkTheme);
+
+// Check for system preference if no preference is saved
+
+if (isDarkTheme === null) {
+    if (colorSchemeQuery.matches) {
+        setDarkTheme(true);
+
+    } else {
+        setDarkTheme(false);
+    }
+} else {
+    setDarkTheme(isDarkTheme);
+}
+
+// Function that sets the theme
+
+function setDarkTheme(isDark) {
+
+    if (documentClasses.length > 0) {
+        documentClasses.remove(...documentClasses);
+    };
+
+    isDarkTheme = isDark;
+    let newTheme = isDark ? "dark" : "light";
+
+    documentClasses.add(newTheme+"-theme");
+    document.documentElement.classList.add(newTheme+'-theme');
+    console.log(newTheme+" theme on");
+}
+
+// Toggle the theme when the button is clicked
+
+function darkThemeButton() {
+    localStorage.setItem("darkTheme", (!isDarkTheme).toString());
+    setDarkTheme(!isDarkTheme);
+}
+
+// Listen for changes in system preference
+
+colorSchemeQuery.addEventListener('change', (event) => {
+    if (event.matches) {
+        setDarkTheme(true);
+    } else {    
+        setDarkTheme(false);
+    }
+});
--- assets/js/language.js
+++ assets/js/language.js
@@ -1,38 +1,12 @@
-
 // Variables
 
 let currentLanguage = localStorage.getItem("language");
-let languagesDropdown = document.getElementById("languages-dropdown");
-let languagesButton = document.getElementById("languages-btn");
+let languagesSelect = document.getElementById("languages-select");
 
-// Translation system
-
+const languages = ['en', 'fr', 'de', 'it'];
 let translations = {};
 
-// Load the translations from the JSON file
-
-async function loadTranslations(lang) {
-  try {
-    const response = await fetch(`../assets/translations/${lang}.json`);
-    if (!response.ok) {
-      throw new Error(`Failed to load translations for ${lang}`);
-    }
-    translations = await response.json();
-    translatePage();
-  } catch (error) {
-    console.error(error);
-  }
-}
-
-// Translate the page content
-
-function translatePage() {
-    for (const translation in translations) {
-        if (document.getElementById(translation) != null){
-            document.getElementById(translation).innerHTML = translations[translation];
-        }
-    }
-}
+// ==========================
 
 // Get the user's browser language
 
@@ -41,44 +15,71 @@ function getUserLanguage() {
     return lang.split("-")[0];
 }
 
+// Function that loads the language options in the DOM
+
+function loadLanguageOptions() {
+    languages.forEach(lang => {
+        const option = document.createElement("option");
+        option.value = lang;
+        option.textContent = lang.toUpperCase();
+        languagesSelect.appendChild(option);
+    });
+}
+
+// Load the translations from the JSON file
+
+async function loadTranslations(lang) {
+    try {
+        const response = await fetch(`../assets/translations/${lang}.json`);
+        if (!response.ok) {
+            throw new Error(`Failed to load translations for ${lang}`);
+        }
+        translations = await response.json();
+        translatePage();
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// Translate the page content
+
+function translatePage() {
+    for (const translation in translations) {
+        if (document.getElementById(translation) != null) {
+            document.getElementById(translation).innerHTML = translations[translation];
+        }
+    }
+}
+
 // Initial setup on page load
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Retrocompatibility with old variables stored & initial language setup
+    // Initial language setup
 
-    if (currentLanguage === null){
-        currentLanguage = getUserLanguage();
-    } else if (currentLanguage.length > 2) {
-        switch (currentLanguage) {
-            case "english":
-                currentLanguage = "en";
-                break;
-            case "french":
-                currentLanguage = "fr";
-                break;
-            case "german":
-                currentLanguage = "de";
-                break;
-            case "italian":
-                currentLanguage = "it";
-                break;
-            default:
-                currentLanguage = getUserLanguage();
-                break;
+    loadLanguageOptions();
+
+    // See of the browser language is supported, otherwise fallback to English
+
+    if (currentLanguage === null) {
+        if (languages.includes(getUserLanguage())){
+            console.log("No user language set, using browser language.");
+            currentLanguage = getUserLanguage();
+        } else {
+            console.log("No user language set, browser language not supported, using English.");
+            currentLanguage = 'en';
         }
-        loadTranslations(currentLanguage);
-    };
+    }
 
     // Load the translations and set the dropdown value
 
     console.log("User language detected as: " + currentLanguage);
     loadTranslations(currentLanguage);
-    document.getElementById("languages-dropdown").value = currentLanguage;
+    languagesSelect.value = currentLanguage;
 
     // Dropdown interactions
 
-    document.getElementById("languages-dropdown").addEventListener("change", (e) => {
+    languagesSelect.addEventListener("change", (e) => {
         loadTranslations(e.target.value);
         localStorage.setItem("language", e.target.value);
         console.log("New user language set: " + e.target.value);
